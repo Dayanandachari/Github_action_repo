36a01bc1f990adf3355cbb225c5c0af398330114
dayanandachari.v <dayanandachari.v@nagarro.com>
2022-10-19 17:42:05 +0530

    pr msg test

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 curl \
      -X POST \
         -H "Accept: application/vnd.github+json" \
-        -H "Authorization: token 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" \
+        -H "Authorization: token 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" \
         https://api.github.com/repos/Dayanandachari/Github_action_repo/issues/5/comments \
         -d'{"body":"**Testing** 123"}'
